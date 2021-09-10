54e658db6c79b36b1198449427f50b6fcc376e93
Luis Carrasco <lcarrasco@guidewire.com>
2021-09-09 22:43:32 -0500

    Change font color for brand orange in Future State Insurance text.

--- components/productAssets/VariantSelector.js
+++ components/productAssets/VariantSelector.js
@@ -5,7 +5,7 @@ const VariantSelector = ({ variantGroups, onSelectOption, selectedOptions, ...pa
     {variantGroups.map(group => (
       <div key={group.id} className="align-items-center">
       <div>{'\u00A0'}</div>
-      <span className="mt-4 mb-4">Add Protection Offered By <span className="font-weight-bold">Future State Insurance</span>?</span>
+      <span className="mt-4 mb-4">Add Protection Offered By <span className="font-fsorange">Future State Insurance</span>?</span>
       <div>{'\u00A0'}</div>
       {group.options.map(option => (
         <button
